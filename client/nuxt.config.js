2259ba1eeadac94b642cca40b904325587b44a18
Mandrican Andrei <adyta094@gmail.com>
2021-11-20 20:39:54 +0200

    change website title in nuxt config

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client',
+    title: 'Giveaway Creator',
     htmlAttrs: {
       lang: 'en'
     },
