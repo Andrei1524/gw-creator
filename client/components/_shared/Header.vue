6e41ceac28fc7043380532be62a310ed4c49620f
Mandrican Andrei <adyta094@gmail.com>
2021-11-20 16:04:22 +0200

    style right nav dropdown

--- client/components/_shared/Header.vue
+++ client/components/_shared/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">
+  <div class="header custom-header">
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
@@ -8,12 +8,14 @@
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown class="header-" right no-caret no-flip>
+          <b-nav-item-dropdown class="custom-dropdown" right no-caret no-flip>
             <template #button-content>
               <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+            <hr class="white-hr">
+            <b-dropdown-item href="#">Logout</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
