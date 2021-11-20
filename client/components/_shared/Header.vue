b11a4cda50288e015b54a570f0b20a1a09b4fae2
Mandrican Andrei <adyta094@gmail.com>
2021-11-20 16:43:34 +0200

    add padding left to header

--- client/components/_shared/Header.vue
+++ client/components/_shared/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header custom-header">
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">Giveaway Creator</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
