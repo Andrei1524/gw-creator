a8b64d9f5780ee3d9aad459573789e10f44312d2
Mandrican Andrei <adyta094@gmail.com>
2021-11-20 18:41:23 +0200

    prettier header.vue

--- client/components/_shared/Header.vue
+++ client/components/_shared/Header.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="header custom-header">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Giveaway Creator</b-navbar-brand>
+  <div class='header custom-header'>
+    <b-navbar toggleable='lg' type='dark' variant='info'>
+      <b-navbar-brand href='#'>Giveaway Creator</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target='nav-collapse'></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse id='nav-collapse' is-nav>
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto d-flex align-items-center">
-          <b-nav-item href="#" class="special-nav-item"><b-icon icon="gift" aria-hidden="true"></b-icon> Create</b-nav-item>
-          <b-nav-item href="#"><b-icon icon="menu-button-wide" aria-hidden="true"></b-icon> Dashboard</b-nav-item>
-          <b-nav-item-dropdown class="custom-dropdown" right no-caret no-flip>
+        <b-navbar-nav class='ml-auto d-flex align-items-center'>
+          <b-nav-item href='#' class='special-nav-item'>
+            <b-icon icon='gift' aria-hidden='true'></b-icon>
+            Create
+          </b-nav-item>
+          <b-nav-item href='#'>
+            <b-icon icon='menu-button-wide' aria-hidden='true'></b-icon>
+            Dashboard
+          </b-nav-item>
+          <b-nav-item-dropdown class='custom-dropdown' right no-caret no-flip>
             <template #button-content>
-              <b-avatar src="https://placekitten.com/300/300"></b-avatar>
+              <b-avatar src='https://placekitten.com/300/300'></b-avatar>
             </template>
-            <b-dropdown-item href="#"><b-icon icon="person-fill" aria-hidden="true"></b-icon> Profile</b-dropdown-item>
-            <b-dropdown-item href="#"><b-icon icon="gear-fill" aria-hidden="true"></b-icon> Settings</b-dropdown-item>
-            <hr class="white-hr">
-            <b-dropdown-item-button><b-icon icon="arrow-bar-left" aria-hidden="true"></b-icon> Logout</b-dropdown-item-button >
+            <b-dropdown-item href='#'>
+              <b-icon icon='person-fill' aria-hidden='true'></b-icon>
+              Profile
+            </b-dropdown-item>
+            <b-dropdown-item href='#'>
+              <b-icon icon='gear-fill' aria-hidden='true'></b-icon>
+              Settings
+            </b-dropdown-item>
+            <hr class='white-hr'>
+            <b-dropdown-item-button>
+              <b-icon icon='arrow-bar-left' aria-hidden='true'></b-icon>
+              Logout
+            </b-dropdown-item-button>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -29,12 +44,19 @@
 export default {
   data() {
     return {}
+  },
+
+  mounted() {
+
+
   }
 }
 </script>
 
-<style lang="scss" scoped>
+
+<style lang='scss' scoped>
 @import './assets/css/variables';
+
 .nav-item {
   margin: 0 0.25em;
 
