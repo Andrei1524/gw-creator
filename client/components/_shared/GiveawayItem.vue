2dae55f37867bd224377f798f2b1263f3edf5927
Mandrican Andrei <adyta094@gmail.com>
2021-12-12 21:31:49 +0200

    Update GiveawayItem.vue

--- client/components/_shared/GiveawayItem.vue
+++ client/components/_shared/GiveawayItem.vue
@@ -1,12 +1,22 @@
 <template>
   <div class='giveaway-item'>
-    item
+    <div class='title'>
+      <span> from nVidia </span>
+      <h5>{{ giveaway.giveaway_name }}</h5>
+    </div>
+    <div class='overlay'></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GiveawayItem',
+  props: {
+    giveaway: {
+      type: Object,
+      required: true
+    }
+  },
 
   data() {
     return {}
@@ -16,6 +26,7 @@ export default {
 
 <style lang='scss'>
 .giveaway-item {
+  position: relative;
   border: 1px solid black;
 }
 </style>
