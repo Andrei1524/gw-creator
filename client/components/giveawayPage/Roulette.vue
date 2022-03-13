75855f7ff82e79c83ec59f945b7b1d02d286c144
Mandrican Andrei <adyta094@gmail.com>
2022-03-14 00:37:12 +0200

    remove old roulette code

--- client/components/giveawayPage/Roulette.vue
+++ client/components/giveawayPage/Roulette.vue
@@ -4,7 +4,7 @@
 
     <div v-show="!loadingRoulette" class='roulette-wrapper'>
       <div v-show='!winner' class='canvas'>
-        <canvas id="c"></canvas>
+        // TODO: redo this with box div's
       </div>
 
       <div v-if='winner'>
@@ -53,16 +53,11 @@ export default {
 
   data() {
     return {
-      requestAnimationFrame: null,
-      cancelAnimationFrame: null,
-      animFrameId: null,
-      resizeEventId: null,
       enrolledUsersData: [],
       rects: [],
       isRouletteRolling: false,
       loadingRoulette: false,
       winner: null,
-      randomWinnerStop: 0
     }
   },
 
@@ -74,186 +69,21 @@ export default {
     }
   },
 
-  watch: {
-    giveaway() {
-      this.resetRoulette()
-    }
-  },
-
-  async mounted() {
-    await this.initCanvasSockets()
+  mounted() {
     // event listeners
     window.addEventListener('resize', _.debounce(() => {
-      if (!this.winner) {
-        this.emitReady()
-        this.initCanvas()
-      }
+      // TODO: adjust generated roulette width
     }, 200))
   },
 
   destroyed() {
-    window.cancelAnimationFrame(this.animFrameId);
     window.removeEventListener('resize', () => {
-      this.emitReady()
-      this.initCanvas()
+      // TODO: adjust generated roulette width
     })
-    this.animFrameId = null
   },
 
   methods: {
     ...mapActions('modules/giveaways', ['getGiveawayEnrolledUsers']),
-
-    async resetRoulette() {
-      this.winner = null
-
-      if (!this.giveaway.rouletteEnded) {
-        await this.initCanvasSockets()
-      } else {
-        this.emitReady()
-      }
-    },
-
-    initCanvasSockets() {
-      // init socket
-      if (!this.giveaway.rouletteEnded) {
-        this.loadingRoulette = true
-        this.socket = this.$nuxtSocket({
-          name: 'home',
-          channel: '/giveaway',
-          reconnection: true
-        })
-
-        // emit ready event
-        this.emitReady()
-
-        this.socket.on('newGeneratedRects', (newGeneratedRects, randomWinnerStop) => {
-          this.rects = randomWinnerStop
-
-          if (randomWinnerStop) {
-            this.isRouletteRolling = true
-            this.randomWinnerStop = randomWinnerStop
-          }
-          this.loadingRoulette = false
-        })
-
-        this.socket.on('winnerRect', winnerRect => {
-          if (winnerRect) {
-            const index = this.rects.findIndex(r => r._id === winnerRect._id)
-            this.rects[index].isWinner = true
-
-            this.winner = winnerRect
-          }
-        })
-
-        this.onConnect()
-
-        // listen to roulette stop events
-        this.socket.on('rouletteEnded', (isRouletteRolling, winner) => {
-          this.isRouletteRolling = isRouletteRolling
-          this.winner = winner
-        })
-      } else { // roulette ended and winner chosen
-        this.winner = this.giveaway.winner
-      }
-    },
-
-    onConnect() {
-      this.socket.on('connect', async () => {
-        await this.initCanvas()
-      })
-    },
-
-    emitReady() {
-      const canvas = document.getElementById("c");
-      this.socket.emit('ready', this.generatedId, canvas.width)
-    },
-
-    async initCanvas() {
-      if (this.animFrameId !== null) {
-        window.cancelAnimationFrame(this.animFrameId);
-        this.animFrameId = null
-      }
-
-      const btn = document.getElementById("pick_winner");
-      const canvas = document.getElementById("c");
-      const ctx = canvas.getContext("2d");
-
-      // const RouletteFunctions = require("./rouletteFunctions");
-
-      canvas.width = document
-        .querySelector(".roulette")
-        .getBoundingClientRect().width;
-      canvas.height = 75;
-      this.rects = [];
-
-      // config
-      const rectWidth = 75;
-
-      // line
-      const line = {
-        x: canvas.width / 2,
-        y: 0,
-        width: 5,
-        height: canvas.height
-      }
-      // ===================
-      let rollWithEase = 0;
-
-      const drawRects = () => {
-        for (let i = 0; i < this.rects.length; i++) {
-          if (!this.rects[i].isWinner) {
-            ctx.fillStyle = "#000";
-            ctx.fillRect(this.rects[i].x + rollWithEase, 0, rectWidth, rectWidth);
-
-            ctx.fillStyle = 'white';
-            ctx.textAlign = "center";
-            ctx.fillText(`${this.rects[i].username}`, this.rects[i].x + rollWithEase + ctx.measureText(`${this.rects[i].username}`).width / 2, rectWidth / 2);
-          } else {
-            ctx.fillStyle = "#FF0000";
-            ctx.fillRect(this.rects[i].x + rollWithEase, 0, rectWidth, 75);
-
-            ctx.fillStyle = 'white';
-            ctx.textAlign = "center";
-            ctx.fillText(`${this.rects[i].username}`, this.rects[i].x + rollWithEase + ctx.measureText(`${this.rects[i].username}`).width / 2, rectWidth / 2);
-          }
-        }
-      }
-
-      // ========================
-      if (this.computeEnrollBtn) {
-        btn.addEventListener("click", () => {
-          this.isRouletteRolling = true
-          this.socket.emit('startSpin', this.generatedId, canvas.width)
-        });
-      }
-
-      await this.socket.on('newGeneratedRects', (newGeneratedRects, randomWinnerStop) => {
-        this.rects = newGeneratedRects
-        this.randomWinnerStop = randomWinnerStop
-      })
-
-      // sockets listener
-      this.socket.on('spin', (data) => {
-        rollWithEase = data + (canvas.width / 2) - this.randomWinnerStop // handle this on server TODO:
-      })
-
-      const drawRouletteWheel = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawRects();
-
-        // draw centered line
-        ctx.rect(line.x, line.y, line.width, line.height);
-        ctx.stroke();
-        ctx.fillStyle = "#fff";
-        ctx.fill();
-
-        // emit ready event for the server
-        // this.socket.emit("ready", this.generatedId) // TODO: fix this, remove from loop
-        this.animFrameId = window.requestAnimationFrame(drawRouletteWheel)
-      }
-
-      drawRouletteWheel();
-    }
   }
 }
 </script>
--- server/src/sockets/sockets.js
+++ server/src/sockets/sockets.js
@@ -5,7 +5,6 @@ function listen(io) {
 
   giveawayNameSpace.on('connection', async (socket) => {
     let room
-    let rollWithEase = 0;
     let rects = []
 
     const rectWidth = 75;
@@ -16,122 +15,15 @@ function listen(io) {
     let foundGiveaway = null
 
     socket.on('ready', async (generatedId, canvasWidth) => {
-      foundGiveaway = await queryGetGiveaway(generatedId)
 
-      // canvas ready
-      room = 'room_' + generatedId
-      socket.join(room)
-
-      if (foundGiveaway.winner && foundGiveaway.isRouletteRolling) {
-        winner = foundGiveaway.winner
-        winner.isWinner = true
-        rects = generateWheelRects(foundGiveaway.enrolled_users, winner, rectWidth, gap)
-        giveawayNameSpace.in(room).emit('newGeneratedRects', rects, foundGiveaway.randomWinnerStop)
-      } else {
-        rects = generateWheelRects(foundGiveaway.enrolled_users, null, rectWidth, gap)
-        giveawayNameSpace.in(room).emit('newGeneratedRects', rects)
-      }
     })
 
     socket.on('startSpin', async (generatedId, canvasWidth) => {
-      // allow spin if user wasn't extracted yet
-      foundGiveaway = await queryGetGiveaway(generatedId)
 
-      if (!foundGiveaway.winner) {
-        await spin(generatedId, canvasWidth)
-      } else {
-        console.log("already extracted")
-      }
     })
 
-    async function spin(generatedId, canvasWidth) {
-      // check if winner has been already extracted
-      const [ winnerFromDraw, winnerXPos ] = draw(foundGiveaway.enrolled_users, rectWidth, gap)
-      winner = winnerFromDraw
+    async function roll(generatedId, canvasWidth) {
 
-      rects = generateWheelRects(foundGiveaway.enrolled_users, winner, rectWidth, gap)
-
-      // update Giveaway winners
-      const updatedGiveaway = await Giveaway.findOneAndUpdate({generatedId: generatedId}, {
-        winner: winner._id,
-        isRouletteRolling: true,
-        randomWinnerStop: Math.floor(Math.random() * (rectWidth - 10)) + 1
-      }, {new:true}).populate('winner')
-
-      // emit the new generated rects with the winner inside
-      giveawayNameSpace.in(room).emit('newGeneratedRects', rects, updatedGiveaway.randomWinnerStop)
-
-      // rollWheel variables
-      let spinTime = 0
-      let spinStart = winnerXPos
-      let spinTimeTotal = 0;
-
-      spinTimeTotal = winnerXPos * 7;
-
-      let timeout = null
-      let immediate = null
-      async function rollWheel() {
-        spinTime += 30;
-
-        if (spinTime >= spinTimeTotal) {
-          await Giveaway.findOneAndUpdate({generatedId: generatedId}, {
-            winner: winner, // TODO: remove this when done testing -> winner: winner
-            rouletteEnded: true, // set to 'true' after testing
-            isRouletteRolling: false
-          })
-
-          giveawayNameSpace.in(room).emit('rouletteEnded', false, updatedGiveaway.winner)
-        } else {
-          const rollTimeWithEase = easeOutQuart(
-            spinTime,
-            0,
-            spinStart,
-            spinTimeTotal
-          );
-          rollWithEase = -rollTimeWithEase;
-
-          giveawayNameSpace.in(room).emit('spin', rollWithEase)
-        }
-      }
-
-      let tickLengthMs = 1000 / 60
-      /* gameLoop related variables */
-      // timestamp of each loop
-      let previousTick = Date.now()
-      // number of times gameLoop gets called
-      let actualTicks = 0
-
-      let gameLoop = function () {
-        let now = Date.now()
-
-        actualTicks++
-        if (previousTick + tickLengthMs <= now) {
-          let delta = (now - previousTick) / 1000
-          previousTick = now
-
-          if (spinTime >= spinTimeTotal) {
-            clearTimeout(timeout)
-            clearImmediate(immediate)
-          } else {
-            // console.log('still rolling')
-            update(delta)
-          }
-          // console.log('delta', delta, '(target: ' + tickLengthMs +' ms)', 'node ticks', actualTicks)
-          actualTicks = 0
-        }
-
-        if (Date.now() - previousTick < tickLengthMs - 16) {
-          timeout = setTimeout(gameLoop)
-        } else {
-          immediate = setImmediate(gameLoop)
-        }
-      }
-
-      let update = function(delta) {
-        rollWheel()
-      }
-
-      gameLoop()
     }
 
     socket.on('disconnect', (reason) => {
@@ -147,34 +39,16 @@ function draw(enrolledUsers, rectWidth, gap) {
   return [enrolledUsers[randomWinnerNr], randomWinnerNr * (rectWidth + gap)]
 }
 
-function generateWheelRects(enrolledUsers, winner, rectWidth, gap) {
-  let rects = []
+function generateWheelDivs(enrolledUsers, winner, rectWidth, gap) {
+  let divs = []
   let startRectX = 0;
 
   for (let i = 0; i < enrolledUsers.length; i++) {
-    if (winner && enrolledUsers[i].username === winner.username) {
-      rects.push({
-        _id: enrolledUsers[i]._id,
-        id: i,
-        x: startRectX,
-        username: enrolledUsers[i].username,
-        isWinner: true
-      });
-    } else {
-      rects.push({
-        _id: enrolledUsers[i]._id,
-        id: i,
-        x: startRectX,
-        username: enrolledUsers[i].username
-      });
-    }
+    // TODO: generate divs
     startRectX += rectWidth + gap;
   }
 
-  return rects
-}
-
-function gameLoop() {
+  return divs
 }
 
 async function queryGetGiveaway(generatedId) {
@@ -184,10 +58,6 @@ async function queryGetGiveaway(generatedId) {
     .populate('winner')
     .select(['-password', '-refreshToken', '-email'])
     .lean().exec()
-}
-
-function easeOutQuart(t, b, c, d) {
-  return -c * ((t = t / d - 1) * t * t * t - 1) + b;
 }
 
 module.exports = {
